1eeafa7a319ecd89b1e5fa78f3ce2da31ad444d8
Marc Hannah <mhannah@highlands.edu>
2017-08-23 07:52:45 -0400

    session6

--- app/Http/Controllers/ServiceRequestController.php
+++ app/Http/Controllers/ServiceRequestController.php
@@ -75,7 +75,7 @@ class ServiceRequestController extends Controller
     public function create()
     {
 //        dd($_SESSION['customer_id']);
-        if (! @)$_SESSION['customer_id'])
+        if (! @$_SESSION['customer_id'])
             return redirect('service_request');
         $customer = Customer::find($id);
         dd($customer);
